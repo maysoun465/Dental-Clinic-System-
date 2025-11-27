d7a5c82543541a4218afdf5ae7db8481d008af58
maysoun <maysounhassan465@gmail.com>
2025-11-27 17:11:27 +0200

    Refactor routing in AllProject component to improve navigation and update placeholder pages

--- dental-clinic/src/AllProject.jsx
+++ dental-clinic/src/AllProject.jsx
@@ -1,14 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Dashboard from "./DoctorDashboard/Dashboard";
+import Dashboard from "./Pages/DoctorPage";
+import LoginPage from "./SignInLogin/LoginPage";
+import SignUpPage from "./SignInLogin/SignUpPage";
+import PatientPage from "./Pages/PatientDashboard";
+import AppointmentsApp from "./Pages/AppointmentsApp";
+import { Navigate } from "react-router-dom";
 
 function AllProject() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/signup" />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} /> 
+
         <Route path="/doctor" element={<Dashboard />} />
         <Route path="/doctor/patients" element={<h1>Patients Page (Coming Soon)</h1>} />
-        <Route path="/doctor/appointments" element={<h1>Appointments Page (Coming Soon)</h1>} />
-        <Route path="*" element={<Dashboard />} />
+        <Route path="/doctor/appointments" element={<AppointmentsApp />} />
+        <Route path="/patient" element={<PatientPage />} />
+
+        <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </BrowserRouter>
   );
